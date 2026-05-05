0dbed1b5a98f415f3cbfde53ecc763e81326f57a
inikiforov <ievgen.nikiforov@tedata.de>
2026-05-05 15:07:08 +0200

    fifixx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,24 +18,48 @@ export const Footer: React.FC<Props> = ({
   onClearCompleted,
 }) => (
   <footer className="todoapp__footer" data-cy="Footer">
-    <span data-cy="TodosCounter">{count} items left</span>
+    <span className="todo-count" data-cy="TodosCounter">
+      {count} items left
+    </span>
 
-    <nav data-cy="Filter">
-      {Object.values(Filter).map(f => (
-        <a
-          key={f}
-          href="#/"
-          className={classNames('filter__link', {
-            selected: selectedFilter === f,
-          })}
-          onClick={() => onFilterChange(f)}
-        >
-          {f}
-        </a>
-      ))}
+    <nav className="filter" data-cy="Filter">
+      <a
+        href="#/"
+        data-cy="FilterLinkAll"
+        className={classNames('filter__link', {
+          selected: selectedFilter === Filter.All,
+        })}
+        onClick={() => onFilterChange(Filter.All)}
+      >
+        All
+      </a>
+
+      <a
+        href="#/active"
+        data-cy="FilterLinkActive"
+        className={classNames('filter__link', {
+          selected: selectedFilter === Filter.Active,
+        })}
+        onClick={() => onFilterChange(Filter.Active)}
+      >
+        Active
+      </a>
+
+      <a
+        href="#/completed"
+        data-cy="FilterLinkCompleted"
+        className={classNames('filter__link', {
+          selected: selectedFilter === Filter.Completed,
+        })}
+        onClick={() => onFilterChange(Filter.Completed)}
+      >
+        Completed
+      </a>
     </nav>
 
     <button
+      type="button"
+      className="todoapp__clear-completed"
       data-cy="ClearCompletedButton"
       onClick={onClearCompleted}
       disabled={!isAnyCompleted}
